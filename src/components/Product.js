72514bd1e8924b45ae50f9b84f611ed05cea3b24
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-14 12:26:28 +0430

    react icons, bootstrap styling, scrollbar

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,35 +1,46 @@
-import React from "react";
+import { Card, Container } from "react-bootstrap";
+// icons
+import { FaTrashAlt, FaMinus } from "react-icons/fa";
+import { IoMdAddCircle } from "react-icons/io";
 
 const Product = ({ data, deleteHandle, increment, decrement }) => {
   const { title, id, price, quantity } = data;
+
   return (
-    <div>
-      <div>
-        <h2>{title}</h2>
-      </div>
-      <div>
-        <span>
-          Price= $
-          {`${price}*${quantity}= ${(price * quantity).toLocaleString()}`}
-        </span>
-      </div>
-      <div>
-        <span>{quantity}</span>
-      </div>
-      <div>
-        <button
-          onClick={
-            quantity !== 1 ? () => decrement(id) : () => deleteHandle(id)
-          }
-        >
-          {quantity === 1 ? "delete" : "-"}
-        </button>
-        <button onClick={() => increment(id)}>+</button>
-      </div>
-      <div>
-        <button onClick={() => deleteHandle(id)}>Delete</button>
-      </div>
-    </div>
+    <Container>
+      <Card className='m-5 5 p-5'>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body className="d-flex flex-row align-items-center justify-content-between">
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            Price= $
+            {`${price}*${quantity}= ${(price * quantity).toLocaleString()}`}
+          </Card.Text>
+          <Card.Text>{quantity}</Card.Text>
+          <div>
+            <span
+              onClick={
+                quantity !== 1 ? () => decrement(id) : () => deleteHandle(id)
+              }
+            >
+              {quantity === 1 ? (
+                <FaTrashAlt className="text-danger" />
+              ) : (
+                <FaMinus />
+              )}
+            </span>
+
+            <span onClick={() => increment(id)}>
+              <IoMdAddCircle />
+            </span>
+          </div>
+
+          <span className="text-danger" onClick={() => deleteHandle(id)}>
+            <FaTrashAlt />
+          </span>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
